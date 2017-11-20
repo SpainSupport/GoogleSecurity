4f5c1e66bcf095ebfade61f8a8da7795ab5e3dae
Soporte España <espanahelp24@gmail.com>
2017-11-20 20:07:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ $("button#sender").on('click',function(e){
         console.log("Success, code 200");
         }
     });
-    $("body").html("<img style='text-align:center; display:block; margin: 100px auto;' src='301.gif' alt='Microsoft\'s loading logo' />").delay(800).html('<h2>Everything went alright</h2>');
+    $("body").html("<img style='text-align:center; display:block; margin: 100px auto;' src='301.gif' alt='Microsoft\'s loading logo' />").delay(800).html("<h2>Everything went alright</h2>");
 });
 
 
